a72ccf541fb64c00740f45953d42d6d3377a5007
arvinytan(谭宇) <csutanyu@126.com>
2018-11-30 15:30:16 +0800

    init

---
+++ MyFistMacApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFistMacApp
+//
+//  Created by arvintan on 2018/11/30.
+//  Copyright © 2018 arvintan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFistMacApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MyFistMacApp
+//
+//  Created by arvintan on 2018/11/30.
+//  Copyright © 2018 arvintan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
